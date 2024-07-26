f681af93ab6978cb19930c53c97c06ea70a1b364
Shashank KATTI <90169675+Shashank26112@users.noreply.github.com>
2024-07-27 01:31:46 +0530

    Add files via upload

---
+++ Shashank Katti Portfolio/scripts.js
@@ -0,0 +1,17 @@
+function toggleDropdown() {
+    var dropdownContent = document.getElementById('dropdown-content');
+    if (dropdownContent.style.display === 'none' || dropdownContent.style.display === '') {
+        dropdownContent.style.display = 'block';
+    } else {
+        dropdownContent.style.display = 'none';
+    }
+}
+
+function toggleDropdown2() {
+    var dropdownContent2 = document.getElementById('dropdown-content2');
+    if (dropdownContent2.style.display === 'none' || dropdownContent2.style.display === '') {
+        dropdownContent2.style.display = 'block';
+    } else {
+        dropdownContent2.style.display = 'none';
+    }
+}
